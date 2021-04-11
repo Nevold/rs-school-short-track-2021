ea9d1c35150377956e49b964932644cd2be25df4
Nevold <nevold@tut.by>
2021-04-12 00:55:38 +0300

    add solution matrixElementsSum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,6 +1,6 @@
 /**
- * Given matrix, a rectangular matrix of integers,
- * just add up all the values that don't appear below a "0".
+  Given matrix, a rectangular matrix of integers,
+  just add up all the values that don't appear below a "0".
  *
  * @param {Array<Array>} matrix
  * @return {Number}
@@ -14,8 +14,20 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new Error('Not implemented');
+  let result = 0;
+  matrix.map((elem, index) => elem.map((e, i) => {
+    if (matrix[index - 1] !== undefined) {
+      if (matrix[index - 1][i] !== 0) {
+        result += e;
+      }
+    } else {
+      result += e;
+    }
+    return result;
+  }));
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
